0c60da0c139d555644705ed7d6cae4de8e94e712
Robert Norlander <robert.norlander@gmail.com>
2011-10-10 19:11:58 +0200

    Added recalculateCost()

--- RouterNode.java
+++ RouterNode.java
@@ -5,6 +5,7 @@ public class RouterNode {
     private GuiTextArea myGUI;
     private RouterSimulator sim;
     private int[] costs = new int[RouterSimulator.NUM_NODES];
+    private int[] routes = new int[RouterSimulator.NUM_NODES];
     private int[][] neighbourVectors = new int[RouterSimulator.NUM_NODES][RouterSimulator.NUM_NODES];
 
     //--------------------------------------------------
@@ -13,25 +14,59 @@ public class RouterNode {
         this.sim = sim;
         myGUI =new GuiTextArea("  Output window for Router #"+ ID + "  ");
 
-        for(int i = 0; i < RouterSimulator.NUM_NODES; i++)
+        for(int i = 0; i < RouterSimulator.NUM_NODES; i++) {
+          routes[i] = i; 
           for(int j = 0; j < RouterSimulator.NUM_NODES; j++) {
             neighbourVectors[i][j] = RouterSimulator.INFINITY;
           }
+        }
 
         System.arraycopy(costs, 0, this.costs, 0, RouterSimulator.NUM_NODES);
 
+        sendVectors();
+    }
+
+    //--------------------------------------------------
+    private void sendVectors() {
         for(int i = 0; i < RouterSimulator.NUM_NODES; i++) {
-          if(costs[i] != RouterSimulator.INFINITY) {
-              RouterPacket pkt = new RouterPacket(myID, i, costs);
-              sendUpdate(pkt);
-          }
+            if(i != myID) {
+                if(costs[i] != RouterSimulator.INFINITY) {
+                    RouterPacket pkt = new RouterPacket(myID, i, costs);
+                    sendUpdate(pkt);
+                }
+            }
         }
+    }
+ 
+
+    //--------------------------------------------------
+    private boolean recalculateCost() {
+        System.out.println("Recalculating costs");  
+
+        boolean dirty = false;
+        for(int i = 0; i < RouterSimulator.NUM_NODES; i++) {
+            for(int dest = 0; dest < RouterSimulator.NUM_NODES; dest++) {
+                int cost = neighbourVectors[i][dest] + neighbourVectors[myID][dest];
+                if(cost < costs[dest]) {
+                    costs[dest] = cost; 
+                    routes[dest] = i;
+                    dirty = true;
+                }
+            }
+        }
+
+        return dirty; 
     }
 
     //--------------------------------------------------
     public void recvUpdate(RouterPacket pkt) {
         System.out.println("recvUpdate");
         neighbourVectors[pkt.sourceid] = pkt.mincost;
+
+        boolean dirty = recalculateCost();
+        if(dirty) {
+            sendVectors();
+        }
     }
 
 
@@ -44,6 +79,7 @@ public class RouterNode {
 
     //--------------------------------------------------
     public void printDistanceTable() {
+        System.out.println("printDistanceTable");
         myGUI.print("Current state for router " + myID + " at time " + sim.getClocktime() + " \n\n");
 
         myGUI.print("Distancetable: \n");
@@ -71,12 +107,45 @@ public class RouterNode {
 
 
         myGUI.print("\n");
+
+        myGUI.println("Our distance vector and routes:");
+
+        myGUI.print("dst");
+        for(int i = 0; i < RouterSimulator.NUM_NODES; i++) {
+            myGUI.print("\t" + i); 
+        }
+
+        myGUI.print("\n");
+
+        for(int i = 0; i < 12*(1+RouterSimulator.NUM_NODES); i++) {
+            myGUI.print("-");
+        }
+        myGUI.print("\n");
+
+        myGUI.print("cost");
+        for(int i = 0; i < RouterSimulator.NUM_NODES; i++) {
+            myGUI.print("\t" + costs[i]);
+        }
+
+        myGUI.print("\n");
+        myGUI.print("route");
+        for(int i = 0; i < RouterSimulator.NUM_NODES; i++) {
+            myGUI.print("\t" + routes[i]);
+        }
+
+        myGUI.print("\n");
+        myGUI.print("\n");
+        myGUI.print("\n");
+ 
     }
 
     //--------------------------------------------------
     public void updateLinkCost(int dest, int newcost) {
-        if(costs[dest] > newcost) {
-          costs[dest] = newcost;
+        neighbourVectors[myID][dest] = newcost;
+
+        boolean dirty = recalculateCost();
+        if(dirty) {
+            sendVectors();
         }
     }
 
